28cdaefd2f32d3ca54f9b35e1a28f17f55a6e024
johncornelles <johncornellews@proton.me>
2024-05-10 15:20:57 +0530

    "Replace custom axios request with useGetMessages hook for fetching messages"
    Explanation:
    The code diff shows the replacement of a custom axios request with the useGetMessages hook to fetch messages. This change simplifies the approact of getting the messages and thus makes the code declarative

--- client/chat-app/src/components/messages/Messages.jsx
+++ client/chat-app/src/components/messages/Messages.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import Message from "./Message";
-import axios from "axios";
 import { useStore } from "../../app/store";
+import useGetMessages from "../../hooks/useGetMessages";
 
 const Messages = () => {
   const selectedConversation = useStore((state) => state.selectedConversation);
   const [messages, setMessages] = useState(null);
+  const { getMessages } = useGetMessages();
 
   useEffect(() => {
-    const get_messages = async () => {
+    const fetchData = async () => {
       if (selectedConversation === null) return;
       try {
-        const res = await axios.get(
-          `http://localhost:7777/api/message/get/${selectedConversation}`,
-          { withCredentials: true }
-        );
-        setMessages(res.data.messages);
+        const res = await getMessages();
+        setMessages(res || []); // Handling the case when res is null
       } catch (error) {
         console.log(error.message);
         setMessages([]);
       }
     };
-    get_messages();
+    fetchData();
   }, [selectedConversation]);
+
   return (
     <div style={{ height: "300px" }} className="px-4 overflow-y-auto">
       {messages &&
