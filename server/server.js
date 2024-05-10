5f61937966eafa34def50837e6dc36b1559d4dea
johncornelles <johncornellews@proton.me>
2024-05-10 15:44:24 +0530

    "Update server.js: add CORS configuration, use array for allowed origins, and import cookie-parser at the beginning"
    This commit updates the server.js file with the following changes:
    
    1. Adds an array of

--- server/server.js
+++ server/server.js
@@ -1,37 +1,42 @@
-import express from "express"
+import express from "express";
 const app = express();
 import dotenv from "dotenv";
 import { authRouter } from "./routes/auth.routes.js";
 import { connectDB } from "./db/connectDB.js";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import msgRouter from "./routes/message.routes.js";
 import userRouter from "./routes/user.routes.js";
-dotenv.config()
-const port = process.env.PORT || 777
-app.use(cookieParser())
+dotenv.config();
+const port = process.env.PORT || 777;
+app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chat-app-john.netlify.app",
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // Replace with your frontend's origin
-    credentials: true, // Allow credentials (cookies) to be sent with requests
+    origin: allowedOrigins,
+    credentials: true,
   })
 );
-app.use(express.json())
 
-app.use("/api/auth", authRouter)
-app.use("/api/message", msgRouter)
-app.use("/api/users",userRouter)
+app.use(express.json());
+
+app.use("/api/auth", authRouter);
+app.use("/api/message", msgRouter);
+app.use("/api/users", userRouter);
 
 const db = async () => {
-    try {
-        await connectDB()
-        app.listen(port, () => {
-            console.log("server is being listened on port", port)
-        })
-    } catch (error) {
-        console.log(error.message)
-    }
-}
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log("server is being listened on port", port);
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-db()
-
+db();
