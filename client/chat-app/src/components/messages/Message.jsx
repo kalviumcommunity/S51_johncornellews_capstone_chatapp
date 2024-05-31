1897cb89ac051df6f28240fa8899ad2c868c870e
johncornelles <johncornellews@proton.me>
2024-05-31 10:50:03 +0530

    "Added date-fns dependency and updated Message component to format timestamps"

--- client/chat-app/src/components/messages/Message.jsx
+++ client/chat-app/src/components/messages/Message.jsx
@@ -1,28 +1,47 @@
 import React from "react";
 import { useStore } from "../../app/store";
+import { format, formatDistanceToNow } from "date-fns";
 
 const Message = ({ message }) => {
   const authUser = useStore().authUser;
   const current_user_id =
     authUser._id || JSON.parse(localStorage.getItem("user"))._id;
 
-  const formattedTime = new Date(message.createdAt).toLocaleString();
+  const createdAt = new Date(message.createdAt);
+  const updatedAt = new Date(message.updatedAt);
+
+  const formattedTime = createdAt;
+  const formattedTime2 = updatedAt;
+
+  const displayTime = (date) => {
+    const diffInHours = (new Date() - date) / (1000 * 60 * 60);
+    if (diffInHours < 1) {
+      // returns an human readable string 
+      return `${formatDistanceToNow(date, { addSuffix: true })}`;
+    }
+    // normal date formatting
+    return format(date, "PPpp");
+  };
 
   return (
-    <>
-      <div
-        className={
-          current_user_id === message.receiverId
-            ? `chat chat-start`
-            : "chat chat-end"
-        }
-      >
-        <div className="chat-bubble">{message.message}</div>
-        <div className="chat-footer">
-          <span>{formattedTime}</span>
-        </div>
+    <div
+      className={`chat ${
+        current_user_id === message.receiverId ? "chat-start" : "chat-end"
+      }`}
+    >
+      <div className="chat-bubble">
+        {message.message}
+        {/* .getTime returns the exact mille seconds of the date  */}
+        {formattedTime.getTime() !== formattedTime2.getTime() && (
+          <p style={{ textAlign: "right" }} className="text-red-400">
+            edited
+          </p>
+        )}
       </div>
-    </>
+      <div className="chat-footer">
+        <p>{displayTime(formattedTime2)}</p>
+      </div>
+    </div>
   );
 };
 
