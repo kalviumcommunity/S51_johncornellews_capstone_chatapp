9eca4ba5af2a6a3e3d2dd0ad2ff567eb4fa4b488
johncornelles <johncornellews@proton.me>
2024-06-01 14:34:00 +0530

    socket functionality is done.
    "Refactor Messages component and useListenMessages hook to handle message deletion and update"

--- client/chat-app/src/hooks/useListenMessages.js
+++ client/chat-app/src/hooks/useListenMessages.js
@@ -7,14 +7,31 @@ export const useListenMessages = () => {
   const setMessages = useStore((state) => state.setMessages);
   const setLatestMessage = useStore((state) => state.setLatestMessage);
   const authUser = useStore().authUser
+  const handleNewMessage = (newMessage) => {
+    setMessages([...messages, newMessage]);
+    const id =
+      newMessage.senderId !== authUser._id
+        ? newMessage.senderId
+        : newMessage.receiverId;
+    setLatestMessage(newMessage, id);
+  };
+  const handleDeleteMessage = (deletedmessage) => {
+    setMessages(
+      messages.filter((message) => message._id !== deletedmessage._id)
+    );
+  };
+  const handleUpdateMessage = (newMessage) => {
+    console.log(newMessage)
+    const updatedMessages = messages.map((msg) =>
+      msg._id === newMessage._id
+        ? { ...msg, message: newMessage.message, updatedAt: new Date() }
+        : msg
+    );
+    setMessages(updatedMessages);
+  }
   useEffect(() => {
-    socket?.on("newMessage", (newMessage) => {
-      setMessages([...messages, newMessage]);
-      const id =
-        newMessage.senderId !== authUser._id
-          ? newMessage.senderId
-          : newMessage.receiverId; 
-      setLatestMessage(newMessage, id); 
-    });
+    socket?.on("newMessage", handleNewMessage);
+    socket?.on("deletemessage", handleDeleteMessage);
+    socket?.on("updatedmessage", handleUpdateMessage);
   }, [socket, messages, setMessages, setLatestMessage]);
 };
