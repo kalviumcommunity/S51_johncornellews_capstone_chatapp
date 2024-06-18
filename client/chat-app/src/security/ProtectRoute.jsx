4b02e88c5180bc7be8e64df85301ff324ae27a2f
johncornelles <johncornellews@proton.me>
2024-06-18 18:25:27 +0530

    "Added jwt cookie check to ProtectRoute component"

--- client/chat-app/src/security/ProtectRoute.jsx
+++ client/chat-app/src/security/ProtectRoute.jsx
@@ -1,10 +1,16 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useStore } from "../app/store.js";
+import Cookies from "js-cookie";
 
 const ProtectRoute = () => {
   const authUser = useStore().authUser;
+  const jwt = Cookies.get("jwt");
   console.log(authUser, "protect");
-  return authUser ? <Outlet /> : <Navigate to="/login" />;
+  return authUser && jwt && jwt !== null && jwt !== undefined ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default ProtectRoute;
