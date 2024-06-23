4907074f16c39b93212840de1102edd598f0fb5d
johncornelles <johncornellews@proton.me>
2024-06-23 14:47:46 +0530

    Added semicolons to end of statements, added comment to explain maxAge value, and added sameSite attribute to cookie options.

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,15 +1,18 @@
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 const generateTokenAndSetCookie = (userID, res) => {
-    const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
-      expiresIn: "15d",
-    });
+  const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
 
-    res.cookie("jwt", token, {
-      maxAge: 15 * 24 * 60 * 60 * 1000, // MS
-      httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-      secure: process.env.NODE_ENV !== "development",
-    });
-    return token
-}
-export default generateTokenAndSetCookie
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days in milliseconds
+    httpOnly: true, // Prevent XSS attacks
+    secure: process.env.NODE_ENV !== "development", // Ensure secure cookie in production
+    sameSite: "None", // Allow cross-site cookies
+  });
+
+  return token;
+};
+
+export default generateTokenAndSetCookie;
