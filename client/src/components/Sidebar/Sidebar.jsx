ea12224675ad49de8c86896dc04fb2f1ecebcda5
johncornelles <johncornellews@proton.me>
2024-07-05 12:29:50 +0530

    side bar and home component done

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,45 +1,55 @@
-import Search from "./Search";
-import Conversations from "./Conversations";
-import LogoutButton from "./LogoutButton";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useStore } from "../../app/store";
-import Profile from "./Profile";
-import Cookies from "js-cookie";
+import React, { useEffect } from 'react';
+import Cookies from 'js-cookie';
+import { useStore } from '../../app/store.js';
+import axios from "axios"
 
 const Sidebar = () => {
-  const users = useStore().users;
-  const filteredUsers = useStore().filteredUsers;
-  const setFilteredUsers = useStore().setFilteredUsers;
-  const setUsers = useStore().setUsers;
-  
+  const jwt = Cookies.get('jwt');
+  const { authUser, setLatestMessage, setUsers, setFilteredUsers } = useStore();
+
+  const fetchlatestMessages = async () => {
+    try {
+      console.log(jwt);
+      const res = await axios.post(`https://s51-john-discuter.onrender.com/message/getlatestmsg/${authUser._id}`, {}, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      console.log(res.data);
+      res.data.forEach((message) => {
+        const id = message.senderId !== authUser._id
+          ? message.senderId
+          : message.receiverId
+        setLatestMessage(message.message, id)
+      })
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const res = await axios.get('https://s51-john-discuter.onrender.com/users/getusers', {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
+      console.log(res.data);
+      setUsers(res.data)
+      setFilteredUsers(res.data)
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   useEffect(() => {
-    const getUsers = async () => {
-      const jwt = Cookies.get("jwt");
-      const res = await axios.post(
-        "https://s51-johncornellews-capstone-chatapp.onrender.com/api/users/getusers",
-        { jwt },
-        { headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-        } }
-      );
-      setUsers(res.data);
-      setFilteredUsers(res.data);
-      console.log(res.data);
-    };
-    return () => getUsers();
+    fetchlatestMessages();
+    getUsers();
   }, []);
 
   return (
     <div>
-      <Profile />
-      <Search users={users} setFilteredUsers={setFilteredUsers} />
-      <div className="divider m-0 p-0" />
-      <Conversations users={filteredUsers} />
-      <div className="divider m-0 p-0" />
-      <LogoutButton />
+      something
     </div>
   );
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,86 +1,20 @@
-import Sidebar from "../../components/Sidebar/Sidebar";
-import MessageContainer from "../../components/messages/MessageContainer";
-import { useEffect } from "react";
-import { useStore } from "../../app/store.js";
-import io from "socket.io-client";
-import axios from "axios";
-import Cookies from "js-cookie";
+import React from 'react'
+import {useStore} from "../app/store.js"
+import Sidebar, {} from "../components/Sidebar/Sidebar.jsx"
+import MessageContainer from '../components/messages/MessageContainer.jsx'
 
 const Home = () => {
-  const setSocket = useStore((state) => state.setSocket);
-  const authUser = useStore((state) => state.authUser);
-  const socket = useStore((state) => state.socket);
-  const setOnlineUsers = useStore((state) => state.setOnlineUsers);
-  const setLatestMessage = useStore().setLatestMessage;
-  const jwt = Cookies.get("jwt");
-  const runner = () => {
-    if (authUser) {
-      const socket = io.connect(
-        "https://s51-johncornellews-capstone-chatapp.onrender.com",
-        {
-          query: {
-            userID: authUser._id,
-          },
-        }
-      );
-      setSocket(socket);
-      socket.on("onlineUsers", (users) => setOnlineUsers(users));
-      return () => socket.close();
-    } else {
-      if (socket) {
-        socket.close();
-        setSocket(null);
-      }
-    }
-  };
-  useEffect(() => {
-    console.log(authUser, "authuser");
-    return () => runner();
-  }, [authUser]);
-
-  useEffect(() => {
-    const setLatestMessages = async () => {
-      try {
-        const res = await axios.post(
-          `https://s51-johncornellews-capstone-chatapp.onrender.com/api/message/getlatestmsg/${authUser._id}`,
-          { jwt },
-          { headers: {
-            "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*"
-          } }
-        );  
-        res.data.forEach((message) => {
-          const id =
-            message.senderId !== authUser._id
-              ? message.senderId
-              : message.receiverId;
-          // Update the latest message for the corresponding user ID
-          setLatestMessage(message, id);
-          console.log(message.message, id);
-        });
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-
-    setLatestMessages();
-  }, [authUser, setLatestMessage]); // Include setLatestMessage in dependencies array
-
-  const selectedConversation = useStore((state) => state.selectedConversation);
-
+  const selectedConversation = useStore().selectedConversation 
   return (
-    <>
-      <div className="flex font-mono">
-        <Sidebar />
-        <div className="divider-vertical m-0 p-0" />
-        {!selectedConversation ? (
-          <h1>Select a convo and start messaging</h1>
-        ) : (
+    <div className='flex font-mono'>
+      <Sidebar />
+      {
+        selectedConversation ? 
           <MessageContainer />
-        )}
-      </div>
-    </>
-  );
-};
+          : <h1 className="text-red-600">select a convo</h1>
+      }
+    </div>
+  )
+}
 
-export default Home;
+export default Home
